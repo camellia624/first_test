750dc4e6b4573b49e22030b12cd95960748a10b0
ricomellia <riko.tsubaki@rizapgroup.com>
2024-08-09 15:20:30 +0900

    except_numの追加

--- test_number.py
+++ test_number.py
@@ -1,5 +1,12 @@
 num = []
+except_num = []
 
 for i in range(100):
     if i % 2 !=0:
         num.append(i)
+    else:
+        except_num.append(i)
+
+
+print(f'num:{num}')
+print(f'except_num:{except_num}')
